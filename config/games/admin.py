53a3424680937af686cc548232605ea12bcd58b0
Jordan Brocker Rudow <jbrockerrudow@gmail.com>
2022-04-14 14:55:13 -0500

    Initial templates to be created for ui

--- config/games/admin.py
+++ config/games/admin.py
@@ -1,4 +1,9 @@
-
 from django.contrib import admin
 
-# Register your models here.
+from games.models import Game, GameTime, GameTimeInvitation, SearchTerm, Genre
+
+admin.site.register(Game)
+admin.site.register(GameTime)
+admin.site.register(GameTimeInvitation)
+admin.site.register(SearchTerm)
+admin.site.register(Genre)
