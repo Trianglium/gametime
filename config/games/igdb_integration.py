183a6be896c2ca63e1342fa498ab1fb263adcf6f
Jordan Brocker Rudow <jbrockerrudow@gmail.com>
2022-04-14 14:55:06 -0500

    IGDB data integration for games app

---
+++ config/games/igdb_integration.py
@@ -0,0 +1,72 @@
+import logging
+import re
+from datetime import timedelta
+
+from django.utils.timezone import now
+
+from games.models import Genre, SearchTerm, Game
+from gt_igdb.django_client import get_client_from_settings
+
+logger = logging.getLogger(__name__)
+
+
+def get_or_create_genres(genre_names):
+    for genre_name in genre_names:
+        genre, created = Genre.objects.get_or_create(igdb_id=genre_name)
+        yield genre
+
+
+def fill_game_details(game):
+    """
+    Fetch a game's full details from igdb. Then, save it to the DB. If the game already has a `full_record` this does
+    nothing, so it's safe to call with any `Game`.
+    """
+    if game.is_full_record:
+        logger.warning(
+            "'%s' is already a full record.",
+            game.name,
+        )
+        return
+    igdb_client = get_client_from_settings()
+    game_details = igdb_client.get_by_id(game.id)
+    game.name = game_details.name
+    game.genres.clear()
+    for genre in get_or_create_genres(game_details.genres):
+        game.genres.add(genre)
+    game.save()
+
+
+def search_and_save(search):
+    """
+    Perform a search for search_term against the API, but only if it hasn't been searched in the past 24 hours. Save
+    each result to the local DB as a partial record.
+    """
+    # Replace multiple spaces with single spaces, and lowercase the search
+    normalized_search_term = re.sub(r"\s+", " ", search.lower())
+
+    search_term, created = SearchTerm.objects.get_or_create(term=normalized_search_term)
+
+    if not created and (search_term.last_search > now() - timedelta(days=1)):
+        # Don't search as it has been searched recently
+        logger.warning(
+            "Search for '%s' was performed in the past 24 hours so not searching again.",
+            normalized_search_term,
+        )
+        return
+
+    igdb_client = get_client_from_settings()
+
+    for igdb_game in igdb_client.search(search):
+        logger.info("Saving game: '%s' / '%s'", igdb_game.name, igdb_game.id)
+        game, created = Game.objects.get_or_create(
+            id=igdb_game.id,
+            defaults={
+                "name": igdb_game.name,
+                "genres": igdb_game.genres,
+            },
+        )
+
+        if created:
+            logger.info("Game created: '%s'", game.name)
+
+    search_term.save()
