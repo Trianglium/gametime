8fa7e4003e5a800dbc61dbb0489dfacc31a5c891
Jordan Brocker Rudow <jbrockerrudow@gmail.com>
2022-04-15 10:44:36 -0500

    API Routing config

---
+++ config/config/api_urls.py
@@ -0,0 +1,13 @@
+
+from django.urls import path, include
+from rest_framework.authtoken import views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+urlpatterns = [
+    path("auth/", include("rest_framework.urls")),
+    path("token-auth/", views.obtain_auth_token),
+    path("jwt/", TokenObtainPairView.as_view(), name="jwt_obtain_pair"),
+    path("jwt/refresh/", TokenRefreshView.as_view(), name="jwt_refresh"),
+    path("gametime-auth/", include("gametime_auth.api.urls")),
+    #path("games/", include("games.api.urls")),
+]
