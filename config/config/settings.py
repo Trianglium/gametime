e7aa4f187cc3c8689003639c6e21f652b7e2c555
Jordan Brocker Rudow <jbrockerrudow@gmail.com>
2022-04-16 08:24:57 -0500

    Update config

--- config/config/settings.py
+++ config/config/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from datetime import timedelta
 from pathlib import Path
 import os
-from . import twitch
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -142,8 +141,6 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 ACCOUNT_ACTIVATION_DAYS = 7
-
-BASE_URL = ""
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
