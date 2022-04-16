c04ea3e4a16bd21f0f5e1573ecc72e832d43f67e
Jordan Brocker Rudow <jbrockerrudow@gmail.com>
2022-04-16 08:25:35 -0500

    Model output examples

---
+++ config/gt_igdb/models/games.py
@@ -0,0 +1,67 @@
+[
+    {
+        "id": 33260,
+        "category": 0,
+        "cover": 129073,
+        "created_at": 1495739021,
+        "external_games": [
+            5526,
+            107828,
+            1927694
+        ],
+        "first_release_date": 1459987200,
+        "game_modes": [
+            1
+        ],
+        "genres": [
+            15,
+            32
+        ],
+        "name": "The Fifth Expedition",
+        "platforms": [
+            3,
+            6
+        ],
+        "release_dates": [
+            77732,
+            77733
+        ],
+        "screenshots": [
+            58357,
+            58358,
+            58359,
+            58360,
+            58361
+        ],
+        "similar_games": [
+            25311,
+            25640,
+            34024,
+            36553,
+            37419,
+            65827,
+            111043,
+            111130,
+            113895,
+            118091
+        ],
+        "slug": "the-fifth-expedition",
+        "status": 4,
+        "summary": "Guide your team of explorers into the treacherous depths of an ancient machine. \n \nOn a mission to restore the old mechanisms to life and to save your people on the surface, who rely on the age old construct for food and shelter.",
+        "tags": [
+            1,
+            268435471,
+            268435488
+        ],
+        "themes": [
+            1
+        ],
+        "updated_at": 1643442946,
+        "url": "https://www.igdb.com/games/the-fifth-expedition",
+        "websites": [
+            33480,
+            33481
+        ],
+        "checksum": "bddffda7-802c-e138-9939-9f961ee98579"
+    }
+]
---
+++ config/gt_igdb/models/genres.py
@@ -0,0 +1,11 @@
+[
+    {
+        "id": 4,
+        "created_at": 1297555200,
+        "name": "Fighting",
+        "slug": "fighting",
+        "updated_at": 1323216000,
+        "url": "https://www.igdb.com/genres/fighting",
+        "checksum": "2ccc6572-bdde-6ed4-8843-25447ea40782"
+    }
+]
